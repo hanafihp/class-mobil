e061b07e417b14509605c9da078036d015aaa046
hanafihp <hanafihanatapraja@students.amikom.ac.id>
2023-03-27 20:24:00 +0700

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Mobil mobilku = new Mobil();
+                mobilku.jumlahPintu = 2;
+                mobilku.warna = "Putih";
+                mobilku.merek = "Toyota";
+                mobilku.model = "Supra MK";
+                mobilku.tahunKeluaran = 2023;
+
+                mobilku.tampilkanInfo(); // Output: "Mobil saya berwarna Putih, merek Toyota model Supra MK keluaran tahun 2023 dengan jumlah pintu sebanyak 2"
+
+                mobilku.gas(320); // Output: "Mobil Supra MK berjalan dengan kecepatan 320"
+
+                mobilku.klakson("pettt"); // Output: "pettt!"
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Mobil
+{
+    public string warna { get; set; }
+    public int jumlahPintu { get; set; }
+    public string merek { get; set; }
+    public string model { get; set; }
+    public int tahunKeluaran { get; set; }
+
+    public void gas(int kecepatan)
+    {
+        Console.WriteLine("Mobil " + model + " berjalan dengan kecepatan " + kecepatan);
+    }
+
+    public void klakson(string suara)
+    {
+        Console.WriteLine(suara);
+    }
+
+    public void tampilkanInfo()
+    {
+        Console.WriteLine("Mobil saya berwarna " + warna + ", merek " + merek + " model " + model + " keluaran tahun " + tahunKeluaran + " dengan jumlah pintu sebanyak " + jumlahPintu);
+    }
+}
